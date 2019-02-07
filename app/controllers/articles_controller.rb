8de1f7abd0501a89b34bf3382bbe89705afcccf8
Adnan Salahuddin <adnan.salah84@gmail.com>
2019-02-07 16:19:13 +0500

    add some validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,32 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def new
-        
+        @article = Article.new
     end
 
     def create
-        render plain: params[:article].inspect
+        # render plain: params[:article].inspect
+        # @article = Article.new(params[:article]) //Error!
+        # @article = Article.new(params.require(:article).permit(:title, :text))
+
+        @article = Article.new(article_parms)
+
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    private
+    def article_parms
+        params.require(:article).permit(:title, :text)
     end
 end
