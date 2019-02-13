517b4b76eba9000b1c855e46fb15bd15fed1b052
Adnan Salahuddin <adnan.salah84@gmail.com>
2019-02-13 16:08:53 +0500

    refactored comment and form html body

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
-    has_many :comments
+    # If you delete an article, its associated comments will also need to be deleted, 
+    # otherwise they would simply occupy space in the database.
+    has_many :comments, dependent: :destroy 
     validates :title, presence: true,
                     length: { minimum: 5 }
 end
