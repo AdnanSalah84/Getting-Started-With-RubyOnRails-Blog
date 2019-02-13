63fc331f1f4e250dc3159e86419a68cc0f56e8cc
Adnan Salahuddin <adnan.salah84@gmail.com>
2019-02-13 13:53:50 +0500

    created controller comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   
   resources :articles  
 
+  # This creates comments as a nested resource within articles. 
+  # This is another part of capturing the hierarchical relationship 
+  # that exists between articles and comments.
+  resources :articles do 
+    resources :comments
+  end
+
   root 'welcome#index'
 end
